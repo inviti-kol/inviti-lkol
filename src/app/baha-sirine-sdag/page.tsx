3abe6bf7370ab58cb111e563b0495651552a9e79
inviti-kol <bkhaskh@i3m.upv.es>
2026-07-30 19:55:13 +0200

    backup antes de nueva invitacion

--- src/app/baha-sirine-sdag/page.tsx
+++ src/app/baha-sirine-sdag/page.tsx
@@ -1,5 +1,36 @@
+import type { Metadata } from "next"
 import BookInvitation from "@/components/Sdag/BookInvitation"
-import "@/style/Sdag.css";
+import "@/style/Sdag.css"
+
+export const metadata: Metadata = {
+  title: "دعوة زفاف بهاء وسيرين",
+  description: "يسرنا دعوتكم لمشاركتنا فرحتنا.",
+
+  openGraph: {
+    title: "دعوة زفاف بهاء وسيرين",
+    description: "يسرنا دعوتكم لمشاركتنا فرحتنا.",
+    url: "https://inviti-lkol.netlify.app/baha-sirine-sdag",
+    siteName: "InvitiKol",
+    images: [
+      {
+        url: "/images/bs.svg", // o b&s.svg si no lo renombras
+        width: 512,
+        height: 512,
+        alt: "B&S",
+      },
+    ],
+    locale: "ar",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "دعوة زفاف بهاء وسيرين",
+    description: "يسرنا دعوتكم لمشاركتنا فرحتنا.",
+    images: ["/images/bs.svg"],
+  },
+}
+
 export default async function Invitation({
   params,
 }: {
@@ -8,7 +39,5 @@ export default async function Invitation({
 
   const { slug } = await params
 
-  return (
-    <BookInvitation />
-  )
+  return <BookInvitation />
 }
