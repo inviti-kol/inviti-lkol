3620376103ac63c223d508efc52f53687174835f
inviti-kol <bkhaskh@i3m.upv.es>
2026-07-30 20:35:36 +0200

    backup antes de nueva invitacion

--- src/app/baha-sirine-sdag/page.tsx
+++ src/app/baha-sirine-sdag/page.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "https://inviti-lkol.netlify.app/images/bs_2.png",
+        url: "https://inviti-lkol.netlify.app/images/bs_2.png?=v1",
         width: 1200,
         height: 630,
         alt: "دعوة عقد القران بهاء وسيرين",
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
     description:
       "يسرنا دعوتكم لحضور عقد قران بهاء وسيرين ومشاركتنا هذه المناسبة السعيدة، حضوركم يزيد فرحتنا ويشرفنا.",
     images: [
-      "https://inviti-lkol.netlify.app/images/bs_2.png",
+      "https://inviti-lkol.netlify.app/images/bs_2.png?=v1",
     ],
   },
 }
